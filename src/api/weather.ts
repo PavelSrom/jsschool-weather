4313882f8eec390f9e83fd3ee39d73df0b656810
Pavel Srom <srompavel98@gmail.com>
2020-11-24 09:32:16 +0100

    more api requests

--- src/api/weather.ts
+++ src/api/weather.ts
@@ -2,9 +2,35 @@ import axios from "axios"
 import { API_CONFIG } from "./config"
 import { CitySearchDTO } from "./types"
 
+// TODO: replace 'any' with types for the remaining requests
+
 export const getCity = (_: string, query: string): Promise<CitySearchDTO[]> =>
   axios
     .get(
       `http://dataservice.accuweather.com/locations/v1/cities/search?q=${query}&apikey=${API_CONFIG.KEY}`
+    )
+    .then(({ data }) => data)
+
+export const getCurrentConditions = (
+  _: string,
+  locationKey: string
+): Promise<any> =>
+  axios
+    .get(
+      `http://dataservice.accuweather.com/currentconditions/v1/${locationKey}?apikey=${API_CONFIG.KEY}`
+    )
+    .then(({ data }) => data)
+
+export const get1DayForecast = (_: string, locationKey: string): Promise<any> =>
+  axios
+    .get(
+      `http://dataservice.accuweather.com/forecasts/v1/daily/1day/${locationKey}?apikey=${API_CONFIG.KEY}`
+    )
+    .then(({ data }) => data)
+
+export const get5DayForecast = (_: string, locationKey: string): Promise<any> =>
+  axios
+    .get(
+      `http://dataservice.accuweather.com/forecasts/v1/daily/5day/${locationKey}?apikey=${API_CONFIG.KEY}`
     )
     .then(({ data }) => data)
